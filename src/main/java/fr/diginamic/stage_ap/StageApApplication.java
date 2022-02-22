3bd2b7b618cc6185fbffd7390210718752f5f575
sopurdey <a_purdey@hotmail.com>
2022-02-22 13:10:22 +0100

    Initialisation

---
+++ src/main/java/fr/diginamic/stage_ap/StageApApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.stage_ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageApApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/stage_ap/StageApApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.stage_ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
